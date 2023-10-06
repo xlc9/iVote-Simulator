66118b7faa474d03d5bd8f77517e6cbcb41a6d24
xlcisneros <xlcisneros03@gmail.com>
2023-10-05 19:56:58 -0700

    Update Student.java & randomize ID

--- VotingService/Student.java
+++ VotingService/Student.java
@@ -1,3 +1,18 @@
-public class Student{
-    
+import java.util.UUID;
+ 
+
+//create parent student class and have subclasses implement their own getAnswer functions
+public abstract class Student{
+    private String id;
+    private String questionType;
+
+    public Student(String qT) {
+        id =UUID.randomUUID().toString();
+        questionType = qT;
+    }
+
+    public String getStudentID() {
+        return id;
+    }
+
 }
