e5c996d99e635362e3448c9a3bf47686453682bf
xlcisneros <xlcisneros03@gmail.com>
2023-10-05 19:55:23 -0700

    Update Question.java

--- VotingService/Question.java
+++ VotingService/Question.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Question {
-    
+    private String questionType;
+    private List<String> candidateAnswers;
+
+    public Question(String qType) {
+        questionType = qType;
+        candidateAnswers = new ArrayList<>();
+    }
+
+
+    //place answer choices into candidateAnswers list
+    public void configureQuestion(List<String> answers) {
+        candidateAnswers.addAll(answers);
+    }
+
+    public String getQuestionType() {
+        return questionType;
+    }
+
+    public List<String> getCandidateAnswers() {
+        return candidateAnswers;
+    }
 }
